5dcc798d6b0cce0643628632a58c0332b698918f
Caleb Vaccaro <CalebVbusiness@gmail.com>
2021-07-12 23:08:34 -0400

    rpi on tests

--- log.py
+++ log.py
@@ -8,7 +8,7 @@ class Log(object):
     file = None
 
     def LogLurk(self):
-        self.file = open("/home/pi/Documents/GRN-HSE/log/log.json", "r")
+        self.file = open("Documents/GRN-HSE/log/log.json", "r")
         self.file.write("\n\n")
         self.file.write(datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
         self.file.write("\n")
@@ -16,7 +16,7 @@ class Log(object):
 
     def StopLog(self):
         SensorLib.LCD.printData("Log","Stop Logging")
-        self.file = open("/home/pi/Documents/GRN-HSE/log/log.json", "a")
+        self.file = open("Documents/GRN-HSE/log/log.json", "a")
         self.file.close()
 
     def LogInfo(self,data):
@@ -25,7 +25,7 @@ class Log(object):
         jsonData = json.dumps(str(data))
 
         # write to file
-        self.file = open("/home/pi/Documents/GRN-HSE/log/log.json", "a")
+        self.file = open("Documents/GRN-HSE/log/log.json", "a")
         self.file.write("\nRuntime " + str(0) + " :" + str(jsonData) + str("\n") + "dt: " + datetime.today().strftime('%H:%M:%S') +"\n")
 
 
